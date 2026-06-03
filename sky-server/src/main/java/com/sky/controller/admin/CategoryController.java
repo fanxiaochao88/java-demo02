7cfbd9bfdf28e7967835a829a5352959d803e0b0
fanxiaochao <2319480743@qq.com>
2026-06-03 17:28:16 +0800

    修改新增菜品和套餐分类bug

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class CategoryController {
      */
     @PostMapping
     @ApiOperation("新增分类")
-    public Result save(CategoryDTO  category) {
+    public Result save(@RequestBody CategoryDTO  category) {
         log.info("新增分类：{}", category);
         categoryService.save(category);
         return Result.success();
