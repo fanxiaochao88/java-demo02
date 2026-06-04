c2206883dd04e60283a9fdf9d9b8d40b42a86b8f
fanxiaochao <2319480743@qq.com>
2026-06-04 23:11:31 +0800

    完成菜品的分页查询

--- sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/DishPageQueryDTO.java
@@ -1,22 +1,30 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel("分页查询参数")
 public class DishPageQueryDTO implements Serializable {
 
+    @ApiModelProperty("页码")
     private int page;
 
+    @ApiModelProperty("每页数据条数")
     private int pageSize;
 
+    @ApiModelProperty("菜品名称")
     private String name;
 
     //分类id
+    @ApiModelProperty("分类id")
     private Integer categoryId;
 
     //状态 0表示禁用 1表示启用
+    @ApiModelProperty("状态 0表示禁用 1表示启用")
     private Integer status;
 
 }
